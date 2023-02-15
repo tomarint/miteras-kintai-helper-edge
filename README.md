8ee543e3fd79e33028be9d0c47ded85488c971d0
tomarin <tomarint@gmail.com>
2023-02-16 01:05:06 +0900

    miteras-kintai-helper v1.0.5

---
+++ README.md
@@ -0,0 +1,21 @@
+# 🚀 MiTERAS勤怠入力
+
+これは、MiTERAS勤怠の休憩時間を自動的に入力するChrome拡張機能です。
+
+![Screenshot](./screenshots/1280x800.png)
+
+## インストール方法
+
+Chromeウェブストアからインストールできます。
+
+<https://chrome.google.com/webstore/detail/miteras-kintai-helper/lnpodkficjcocjggmhjeckdlfjkjndhg>
+
+## 初期設定
+
+1. アドオンのオプション画面を開いて、休憩時間の設定を行ってください。
+
+## 使用方法
+
+1. MiTERAS勤怠上の休憩時間の入力画面でコンテキストメニューを表示する。（通常は右クリック）
+1. コンテキストメニューから「MiTERAS勤怠入力」を選択する。
+1. 休憩時間が自動的に入力されます。
---
+++ src/background.ts
@@ -0,0 +1,216 @@
+(() => {
+  class TabManager {
+    readonly filterUrl: string = "https://kintai.miteras.jp/";
+    matchedTab: { [key: number]: boolean; } = {};
+    isUrlMatched(url?: string): boolean {
+      if (url != null && url.indexOf(this.filterUrl) >= 0) {
+        return true;
+      }
+      return false;
+    }
+    updateTab(tabId: number | undefined, url: string | undefined): void {
+      if (tabId == null || tabId == null || tabId < 0 || url == null) {
+        return;
+      }
+      if (this.isUrlMatched(url)) {
+        this.matchedTab[tabId] = true;
+        // console.log(`Tab ${tabId} is changed to in. (url: ${url})`);
+      } else {
+        this.matchedTab[tabId] = false;
+        // console.log(`Tab ${tabId} is changed to out. (url: ${url})`);
+      }
+    }
+    deleteTab(tabId: number): void {
+      delete this.matchedTab[tabId];
+      // console.log(`Tab ${tabId} is deleted.`);
+    }
+    queryTab(tabId: number): boolean {
+      if (this.matchedTab[tabId]) {
+        // console.log(`Tab ${tabId} is in.`);
+        return true;
+      } else {
+        // console.log(`Tab ${tabId} is out.`);
+        return false;
+      }
+    }
+  };
+  class MiterasKintaiHelperBackend {
+    readonly contextMenuId = "miteras-kintai-helper-context-menu-id";
+    readonly messageName = "miteras-kintai-helper-message";
+    tabManager: TabManager = new TabManager();
+    activeTabId: number = -1;
+    options: { breaktime1: number, breaktime2: number, breaktime3: number, loginButtonAnimation: number } = {
+      breaktime1: 705,
+      breaktime2: -1,
+      breaktime3: 0,
+      loginButtonAnimation: 0,
+    };
+
+    updateContextMenu(): void {
+      if (this.activeTabId === -1) {
+        return;
+      }
+      const tabId = this.activeTabId;
+      const isMatched = this.tabManager.queryTab(tabId);
+      // console.log(`updateContextMenus - ${isMatched}`);
+      if (isMatched) {
+        chrome.contextMenus.create(
+          {
+            id: this.contextMenuId,
+            title: chrome.i18n.getMessage("extName"),
+            contexts: ["page"]
+          },
+          () => {
+            if (chrome.runtime.lastError) {
+              // console.log(chrome.runtime.lastError);
+              return;
+            }
+          }
+        );
+      }
+      else {
+        chrome.contextMenus.remove(this.contextMenuId, () => {
+          if (chrome.runtime.lastError) {
+            // console.log(chrome.runtime.lastError);
+            return;
+          }
+        });
+      }
+    }
+
+    constructor() {
+      // Fired when a tab is updated.
+      chrome.tabs.onUpdated.addListener((tabId: number, changeInfo: chrome.tabs.TabChangeInfo, tab: chrome.tabs.Tab) => {
+        if (tab == null || tabId < 0) {
+          return;
+        }
+        if (changeInfo.url != null) {
+          // console.log("onUpdated: " + tab.id, JSON.stringify(changeInfo));
+          this.tabManager.updateTab(tabId, changeInfo.url);
+          this.updateContextMenu();
+        }
+        if (changeInfo.status === "complete") {
+          // console.log("onUpdated: " + tab.id, JSON.stringify(changeInfo), tab.url);
+          this.tabManager.updateTab(tabId, tab.url);
+          this.updateContextMenu();
+          if (this.tabManager.queryTab(tabId)) {
+            chrome.scripting.executeScript(
+              {
+                target: { tabId },
+                files: ["foreground.js"]
+              }
+            ).then((value: chrome.scripting.InjectionResult[]) => {
+              if (chrome.runtime.lastError) {
+                // console.log(chrome.runtime.lastError);
+              }
+            }).catch((reason: any) => {
+              if (chrome.runtime.lastError) {
+                // console.log(chrome.runtime.lastError);
+              }
+            })
+            chrome.storage.sync.get(
+              {
+                breaktime1: '705',
+                breaktime2: '-1',
+                breaktime3: '0',
+                loginButtonAnimation: '0',
+              }
+            ).then((items) => {
+              this.options = {
+                breaktime1: Number(items.breaktime1),
+                breaktime2: Number(items.breaktime2),
+                breaktime3: Number(items.breaktime3),
+                loginButtonAnimation: Number(items.loginButtonAnimation),
+              };
+              if (this.options.loginButtonAnimation !== 1) {
+                chrome.scripting.insertCSS(
+                  {
+                    target: { tabId },
+                    css: `.login-button { transition: none !important; }`
+                  }
+                ).then(() => {
+                  if (chrome.runtime.lastError) {
+                    // console.log(chrome.runtime.lastError);
+                  }
+                }).catch((reason: any) => {
+                  if (chrome.runtime.lastError) {
+                    // console.log(chrome.runtime.lastError);
+                  }
+                })
+              }
+            }).catch(function (error) {
+              console.log(error);
+            });
+          }
+        }
+      });
+
+      // Fires when the active tab in a window changes.
+      // Note that the tab's URL may not be set at the time this event fired, but you can listen to onUpdated events to be notified when a URL is set.
+      chrome.tabs.onActivated.addListener((activeInfo: chrome.tabs.TabActiveInfo) => {
+        if (activeInfo != null && activeInfo.tabId != null) {
+          this.activeTabId = activeInfo.tabId;
+          this.updateContextMenu();
+        }
+      });
+
+      // Fired when a tab is created.
+      // Note that the tab's URL may not be set at the time this event fired, but you can listen to onUpdated events to be notified when a URL is set.
+      chrome.tabs.onCreated.addListener((tab: chrome.tabs.Tab) => {
+        this.tabManager.updateTab(tab.id, tab.url);
+      });
+
+      // Fired when a tab is closed.
+      chrome.tabs.onRemoved.addListener((tabId: number, removeInfo: chrome.tabs.TabRemoveInfo) => {
+        this.tabManager.deleteTab(tabId);
+      });
+
+      // Fired when the currently focused window changes.
+      // Will be chrome.windows.WINDOW_ID_NONE if all chrome windows have lost focus.
+      chrome.windows.onFocusChanged.addListener((windowId: number) => {
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs: chrome.tabs.Tab[]) => {
+          if (tabs.length > 0) {
+            for (const tab of tabs) {
+              if (tab != null && tab.id != null && tab.id >= 0) {
+                this.tabManager.updateTab(tab.id, tab.url);
+              }
+            }
+            if (tabs[0].id != null && tabs[0].id >= 0) {
+              this.activeTabId = tabs[0].id;
+              this.updateContextMenu();
+            }
+          }
+        });
+      });
+
+      // Fired when a context menu item is clicked.
+      chrome.contextMenus.onClicked.addListener((info: chrome.contextMenus.OnClickData, tab?: chrome.tabs.Tab) => {
+        // console.log('context menu clicked');
+        // console.log(info);
+        // console.log(tab);
+        if (tab == null || tab.id == null || tab.id < 0) {
+          return;
+        }
+        if (info.menuItemId === this.contextMenuId) {
+          chrome.tabs.sendMessage(
+            tab.id,
+            {
+              message: this.messageName
+            },
+            (response) => {
+              if (chrome.runtime.lastError) {
+                // console.log("chrome.runtime.lastError: ", chrome.runtime.lastError);
+                return;
+              }
+              if (response != null && response.message === "success") {
+                // console.log("contextMenu is succeeded.");
+              }
+            }
+          );
+          return true;
+        }
+      });
+    }
+  };
+  new MiterasKintaiHelperBackend();
+})();
