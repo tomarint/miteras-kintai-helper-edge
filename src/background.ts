d833484a28846a39d926e3d0d0872ff95153d92a
tomarin <tomarint@gmail.com>
2023-02-21 00:35:28 +0900

    yarn upgrade-interactive --latest

--- src/background.ts
+++ src/background.ts
@@ -99,7 +99,7 @@
                 target: { tabId },
                 files: ["foreground.js"]
               }
-            ).then((value: chrome.scripting.InjectionResult[]) => {
+            ).then(() => {
               if (chrome.runtime.lastError) {
                 // console.log(chrome.runtime.lastError);
               }
